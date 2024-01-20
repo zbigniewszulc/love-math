a98d4ead12275604467adba99abca0d4f2367a4f
Zbigniew Szulc <zbigniewszulc@gmail.com>
2024-01-20 20:51:21 +0000

    Add favicon, clear answer box every time game is run, change title of the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,9 @@ document.addEventListener("DOMContentLoaded", function(){
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+
     // Creates two random numbers between 1 and 25
     let num1 =  Math.floor(Math.random() * 25) + 1;
     let num2 =  Math.floor(Math.random() * 25) + 1;
