bc310306a99d6b89b7bed38d0c076fc7c72de45b
Zbigniew Szulc <zbigniewszulc@gmail.com>
2024-01-20 18:21:51 +0000

    Add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,6 +29,8 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+        displayMutiplyQuestion(num1, num2);
     } else {
         alert(`Uknown game type: ${gameType}`);
         throw `Uknown game type: ${gameType} . Aborting!`;
@@ -65,7 +67,9 @@ function calculateCorrectAnswer() {
     let operator = document.getElementById('operator').innerText;
 
     if (operator === "+") {
-        return [operand1, operand2, "addition"];
+        return [operand1 + operand2, "addition"];
+    } else if(operator === "x") {
+        return [operand1 * operand2, "multiply"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -98,6 +102,8 @@ function displaySubstractQuestion() {
     
 }
 
-function displayMutiplyQuestion() {
-    
+function displayMutiplyQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
 }
